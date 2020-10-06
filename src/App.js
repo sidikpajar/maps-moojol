dc78f865480adc77ba14f7a2f2c18a54c57038f1
noops96 <stvincen1196@gmail.com>
2020-10-06 15:00:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={12}
+    defaultCenter={{ lat: -6.200000, lng: 106.816666 }}
+  >
+    {
+      props.region.map((data,index)=>{
+        const helo = 'helo';
+        return(
+          <Marker
+            position={{ lat: data.x, lng: data.y }}
+          />
+        )
+      })
+    }
+    
+  </GoogleMap>
+));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      region:[
+        {
+          lokasiid:64100,
+          x: -6.1161274, 
+          y: 106.7631043
+        },
+        {
+          lokasiid:64101,
+          x: -6.2128734, 
+          y: 106.7837462
+        },
+      ]
+    };
+  }
+  render() {
+    return (
+      <div style={{width:"100vw", height:"100vh"}}>
+        <MapWithAMarker
+          region={this.state.region}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyD4DtpjgzXjI5q-ZjmDObB7r3Tw3Q7xl58&v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{height:"100%"}}/>}
+          containerElement={<div style={{height:"100%"}}/>}
+          mapElement={<div style={{height:"100%"}}/>}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
